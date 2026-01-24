f665560028b620c37834381df41efac49a97026a
Russkkk <154346835+Russkkk@users.noreply.github.com>
2026-01-24 12:20:55 -0300

    Add files via upload

---
+++ popular_db.py
@@ -0,0 +1,22 @@
+import sqlite3
+import pandas as pd
+
+DB_FILE = "leads.db"
+
+df = pd.read_csv("leads_teste.csv")
+
+conn = sqlite3.connect(DB_FILE)
+cursor = conn.cursor()
+
+# Limpa tabela se quiser recomeçar (opcional)
+# cursor.execute("DELETE FROM leads")
+
+for _, row in df.iterrows():
+    cursor.execute("""
+        INSERT INTO leads (tempo_site, paginas_visitadas, clicou_preco, virou_cliente)
+        VALUES (?, ?, ?, ?)
+    """, (row['tempo_site'], row['paginas_visitadas'], row['clicou_preco'], row['virou_cliente']))
+
+conn.commit()
+conn.close()
+print("Dados inseridos com sucesso!")
